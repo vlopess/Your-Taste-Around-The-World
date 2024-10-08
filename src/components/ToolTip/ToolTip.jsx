d520a0ba5967b37357a27cc81377b2c3a4cd54dc
victor <victorlopes9291@gmail.com>
2024-10-07 22:29:21 -0300

    feat: adicionando o componente tooltip

---
+++ src/components/ToolTip/ToolTip.jsx
@@ -0,0 +1,14 @@
+import "./ToolTip.css";
+
+export const ToolTip = () => {
+    return (
+        <>
+            <div class="tooltip-container">
+              <span class="tooltip">
+                Todas as informações sobre a localização dos artistas foram obtidas através de uma IA, portanto, estão sujeitas a erros.
+              </span>
+              <span class="text">?</span>
+            </div>
+        </>
+    )
+}

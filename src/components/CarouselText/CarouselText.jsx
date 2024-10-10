9904aed4733874f14808412323214fbcf51846cf
vlopes <victorlopes9291@gmail.com>
2024-10-10 15:58:28 -0300

    fix: ajustes no Carousel

--- src/components/CarouselText/CarouselText.jsx
+++ src/components/CarouselText/CarouselText.jsx
@@ -9,7 +9,7 @@ export const CarouselText = () => {
     useEffect(() => {
         setTimeout(() => {
             let newIndex = (index + 1);
-            if(newIndex >= 4) newIndex = 0;
+            if(newIndex >= 4) newIndex = 1;
             setIndex(newIndex);
         }, 3000);
     }, [index]);
@@ -17,52 +17,56 @@ export const CarouselText = () => {
 
     return (
         <>
-                <div style={{width: "50%"}}>
-                    <AnimatePresence>
-                        {index === 1 &&(
-                            <motion.div
-                                initial={{x: 300, opacity: 0}}
-                                animate={{x: 0, opacity: 1}}
-                                exit={{x: "-100vw", opacity: 0, transition: { duration: 0.7 }}}
-                            >
-                                <p>Bem-vindo ao <i>Your Taste Around The World</i></p>
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
-                    <AnimatePresence>
-                        {index === 2 && (
-                            <motion.div
-                                initial={{x: 300, opacity: 0}}
-                                animate={{x: 0, opacity: 1}}
-                                exit={{x: "-100vw", opacity: 0, transition: { duration: 0.7 }}}
-                            >
-                                <p>Você já se perguntou de onde vêm os artistas que você mais escuta?</p>
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
-                    <AnimatePresence>
-                        {index === 3 && (
-                            <motion.div
-                                initial={{x: 300, opacity: 0}}
-                                animate={{x: 0, opacity: 1}}
-                                exit={{x: "-100vw", opacity: 0, transition: { duration: 0.7 }}}
-                            >
-                                <p>Aqui, você vai descobrir a origem dos seus artistas favoritos!</p>
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
-                    <AnimatePresence>
-                        {index === 4 && (
-                            <motion.div
-                                initial={{x: 300, opacity: 0}}
-                                animate={{x: 0, opacity: 1}}
-                                exit={{x: "-100vw", opacity: 0, transition: { duration: 0.7 }}}
-                            >
-                                <p>Com sua conta do LastFm ou Spotify, você pode descobrir tudo isso!</p>
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
-                </div>
+            <div style={{width: "50%"}}>
+                <AnimatePresence>
+                    {index === 1 && (
+                        <motion.div
+                            className={"txt"}
+                            initial={{x: 100, opacity: 0}}
+                            animate={{opacity: 1}}
+                            exit={{y: "-10vw", x: 100, opacity: 0, transition: {duration: 0.7}}}
+                        >
+                            <p>Bem-vindo ao <i>Your Taste Around The World</i></p>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+                <AnimatePresence>
+                    {index === 2 && (
+                        <motion.div
+                            className={"txt"}
+                            initial={{x: 100, opacity: 0}}
+                            animate={{opacity: 1}}
+                            exit={{y: "-10vw", x: 100, opacity: 0, transition: {duration: 0.7}}}
+                        >
+                            <p>Você já se perguntou de onde vêm os artistas que você mais escuta?</p>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+                <AnimatePresence>
+                    {index === 3 && (
+                        <motion.div
+                            className={"txt"}
+                            initial={{x: 100, opacity: 0}}
+                            animate={{opacity: 1}}
+                            exit={{y: "-10vw", x: 100, opacity: 0, transition: {duration: 0.7}}}
+                        >
+                            <p>Aqui, você vai descobrir a origem dos seus artistas favoritos!</p>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+                <AnimatePresence>
+                    {index === 4 && (
+                        <motion.div
+                            className={"txt"}
+                            initial={{x: 100, opacity: 0}}
+                            animate={{opacity: 1}}
+                            exit={{y: "-10vw", x: 100, opacity: 0, transition: {duration: 0.7}}}
+                        >
+                            <p>Com sua conta do LastFm ou Spotify, você pode descobrir tudo isso!</p>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </div>
         </>
     )
 }

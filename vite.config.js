2ed12afc83414d2a70a5e339efc753b70da723d7
Victor Lopes <121266927+vlopess@users.noreply.github.com>
2024-10-12 08:20:17 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Your-Taste-Around-The-World/",
+  base: "/Your-Taste-Around-The-World",
   build: {
     outDir: "dist",
   },
